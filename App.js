260d1a3692a89da219a1572c14ce1fd3cef705d0
Hz <haozhe4533@outlook.com>
2021-11-13 00:54:35 +0800

    Update HOme Page

--- App.js
+++ App.js
@@ -4,19 +4,12 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import HomePage from "./src/HomePage";
 import StartPage from "./src/StartPage";
 import RegisterPage from "./src/RegisterPage";
 import LoginPage from "./src/LoginPage";
 import RegisterSuccessfulPage from "./src/RegisterSuccessfulPage";
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Details!</Text>
-    </View>
-  );
-}
 
 function SettingsScreen({ navigation }) {
   return (
@@ -30,60 +23,35 @@ function SettingsScreen({ navigation }) {
   );
 }
 
-const HomeStack = createNativeStackNavigator();
-
-function HomeStackScreen() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
-    </HomeStack.Navigator>
-  );
-}
-
-const SettingsStack = createNativeStackNavigator();
-
-function SettingsStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-      <SettingsStack.Screen name="Details" component={DetailsScreen} />
-    </SettingsStack.Navigator>
-  );
-}
-
 const Tab = createBottomTabNavigator();
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back Home" />
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-{
-  /* <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator> */
-}
 export default function App() {
   return (
     <NavigationContainer>
-      <RegisterSuccessfulPage />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused
+                ? "ios-list-circle-sharp"
+                : "ios-list-circle-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#009ca7",
+          tabBarInactiveTintColor: "black",
+          headerTitleAlign: "center",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,21 +1,64 @@
-import * as React from "react";
-import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Btn from "./component/MainButton";
-
-const Separator = () => <View style={styles.separator} />;
+import React, { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
+import { Searchbar, Card, Title, Paragraph } from "react-native-paper";
+import logo from "../assets/homeIcon.png";
+import { ImageSlider } from "react-native-image-slider-banner";
 
 function HomePage({ navigation }) {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const onChangeSearch = (query) => setSearchQuery(query);
+
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Searchbar
+          style={styles.search}
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </View>
+
+      <View style={styles.imageSliderContainer}>
+        <ImageSlider
+          data={[
+            {
+              img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5a5uCP-n4teeW2SApcIqUrcQApev8ZVCJkA&usqp=CAU",
+            },
+            {
+              img: "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
+            },
+            {
+              img: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
+            },
+          ]}
+          autoPlay={false}
+          onItemChanged={(item) => console.log("item", item)}
+          closeIconColor="#fff"
+          caroselImageStyle={{ height: 150 }}
+        />
+      </View>
+
+      <View style={styles.categoryContainer}>
+        <Card style={styles.category}>
+          <Card.Content>
+            <Title>Card title</Title>
+            <Paragraph>Card content</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card style={styles.category}>
+          <Card.Content>
+            <Title>Card title</Title>
+            <Paragraph>Card content</Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+
       <View>
         <Text style={styles.title}>
           The title and onPress handler are required. It is recommended to set
           accessibilityLabel to help make your app usable by everyone.
         </Text>
-
-        <Btn title="Sign Up" />
       </View>
     </SafeAreaView>
   );
@@ -23,9 +66,35 @@ function HomePage({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    textAlign: "center",
+    alignItems: "center",
     justifyContent: "center",
-    marginHorizontal: 16,
+    backgroundColor: "#fff",
+  },
+  searchContainer: {
+    backgroundColor: "#fff",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  imageSliderContainer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: "90%",
+  },
+  categoryContainer: {
+    letterSpacing: 10,
+    paddingLeft: 0,
+    flexDirection: "row",
+  },
+  category: {
+    margin: 10,
+    paddingLeft: 0,
+    width: "33.33%",
+    height: 100,
+  },
+  search: {
+    backgroundColor: "#e4f3f4",
+    width: "90%",
   },
   title: {
     textAlign: "center",
@@ -34,11 +103,6 @@ const styles = StyleSheet.create({
   fixToText: {
     flexDirection: "row",
     justifyContent: "space-between",
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: "#737373",
-    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
