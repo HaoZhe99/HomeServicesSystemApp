5fed6c37ac6fb917aff50640ca6da3d654f943f7
Hz <haozhe4533@outlook.com>
2021-11-14 16:45:30 +0800

    Update Home Page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,65 +1,130 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
 import { Searchbar, Card, Title, Paragraph } from "react-native-paper";
 import logo from "../assets/homeIcon.png";
 import { ImageSlider } from "react-native-image-slider-banner";
+
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
 
 function HomePage({ navigation }) {
   const [searchQuery, setSearchQuery] = React.useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
 
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.searchContainer}>
-        <Searchbar
-          style={styles.search}
-          placeholder="Search"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-        />
-      </View>
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={styles.container}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <View style={styles.searchContainer}>
+          <Searchbar
+            style={styles.search}
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
 
-      <View style={styles.imageSliderContainer}>
-        <ImageSlider
-          data={[
-            {
-              img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5a5uCP-n4teeW2SApcIqUrcQApev8ZVCJkA&usqp=CAU",
-            },
-            {
-              img: "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
-            },
-            {
-              img: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
-            },
-          ]}
-          autoPlay={false}
-          onItemChanged={(item) => console.log("item", item)}
-          closeIconColor="#fff"
-          caroselImageStyle={{ height: 150 }}
-        />
-      </View>
+        <View style={styles.imageSliderContainer}>
+          <ImageSlider
+            data={[
+              {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5a5uCP-n4teeW2SApcIqUrcQApev8ZVCJkA&usqp=CAU",
+              },
+              {
+                img: "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
+              },
+              {
+                img: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
+              },
+            ]}
+            autoPlay={false}
+            onItemChanged={(item) => console.log("item", item)}
+            closeIconColor="#fff"
+            caroselImageStyle={{ height: 150 }}
+          />
+        </View>
 
-      <View style={styles.categoryContainer}>
-        <Card style={styles.category}>
-          <Card.Content>
-            <Title>Card title</Title>
-            <Paragraph>Card content</Paragraph>
-          </Card.Content>
-        </Card>
-        <Card style={styles.category}>
-          <Card.Content>
-            <Title>Card title</Title>
-            <Paragraph>Card content</Paragraph>
-          </Card.Content>
-        </Card>
-      </View>
+        <View style={styles.categoryContainer}>
+          <Card style={styles.category}>
+            <Card.Content>
+              <View style={styles.ImageContainer}>
+                <Image source={logo} style={{ width: 50, height: 50 }} />
+              </View>
+              <Title>Card title</Title>
+            </Card.Content>
+          </Card>
+          <Card style={styles.category}>
+            <Card.Content>
+              <View style={styles.ImageContainer}>
+                <Image source={logo} style={{ width: 50, height: 50 }} />
+              </View>
+              <Title>Card title</Title>
+            </Card.Content>
+          </Card>
+          <Card style={styles.category}>
+            <Card.Content>
+              <View style={styles.ImageContainer}>
+                <Image source={logo} style={{ width: 50, height: 50 }} />
+              </View>
+              <Title>Card title</Title>
+            </Card.Content>
+          </Card>
+        </View>
 
-      <View>
-        <Text style={styles.title}>
-          The title and onPress handler are required. It is recommended to set
-          accessibilityLabel to help make your app usable by everyone.
-        </Text>
-      </View>
+        <View style={styles.categoryContainer}>
+          <Card style={styles.category}>
+            <Card.Content>
+              <View style={styles.ImageContainer}>
+                <Image source={logo} style={{ width: 50, height: 50 }} />
+              </View>
+              <Title>Card title</Title>
+            </Card.Content>
+          </Card>
+          <Card style={styles.category}>
+            <Card.Content>
+              <View style={styles.ImageContainer}>
+                <Image source={logo} style={{ width: 50, height: 50 }} />
+              </View>
+              <Title>Card title</Title>
+            </Card.Content>
+          </Card>
+          <Card style={styles.category}>
+            <Card.Content>
+              <View style={styles.ImageContainer}>
+                <Image source={logo} style={{ width: 50, height: 50 }} />
+              </View>
+              <Title>Card title</Title>
+            </Card.Content>
+          </Card>
+        </View>
+
+        <View style={styles.companyDetailsContainer}>
+          <Image source={logo} style={{ width: 140, height: 140 }} />
+          <View style={styles.companyDetailsText}>
+            <Text numberOfLines={8}>Card</Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -69,10 +134,10 @@ const styles = StyleSheet.create({
     textAlign: "center",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#F0F0F0",
   },
   searchContainer: {
-    backgroundColor: "#fff",
+    backgroundColor: "#F0F0F0",
     paddingTop: 10,
     paddingBottom: 10,
   },
@@ -80,29 +145,80 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 10,
     width: "90%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  ImageContainer: {
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
   categoryContainer: {
     letterSpacing: 10,
     paddingLeft: 0,
     flexDirection: "row",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  companyDetailsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 10,
+    width: "90%",
+    height: 140,
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  companyDetailsText: {
+    margin: 10,
+    maxWidth: 190,
   },
   category: {
-    margin: 10,
+    margin: "1%",
     paddingLeft: 0,
-    width: "33.33%",
+    width: "29%",
     height: 100,
+    backgroundColor: "#e4f3f4",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
   },
   search: {
     backgroundColor: "#e4f3f4",
     width: "90%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
   },
   title: {
     textAlign: "center",
     marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: "row",
-    justifyContent: "space-between",
   },
 });
 
